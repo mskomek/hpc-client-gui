b84d38f53a87f71dd2982f9f9be40ce577e8f4a2
mskomek <mskomek@gmail.com>
2026-09-02 22:40:16 +0300

    Add wx remote folder creation

--- tests/test_wx_remote_files.py
+++ tests/test_wx_remote_files.py
@@ -50,3 +50,4 @@ def test_remote_view_runs_operations_off_the_wx_thread():
     assert 'action in {"rename", "copy", "move"}' in source and "dirs.destination" in source
     assert 'action == "download"' in source and "wx.DirDialog" in source
     assert 'action == "upload"' in source and "wx.FileDialog" in source
+    assert 'action == "new_folder"' in source and "wx.TextEntryDialog" in source
